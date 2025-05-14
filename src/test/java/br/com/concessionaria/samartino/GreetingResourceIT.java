9ed8e9881f1495bef1c6845ffd9e532257a63e21
kauetrnt <rm559317@fiap.com.br>
2025-05-14 00:20:14 -0300

    Iniciando Projeto Quarkus

---
+++ src/test/java/br/com/concessionaria/samartino/GreetingResourceIT.java
@@ -0,0 +1,8 @@
+package br.com.concessionaria.samartino;
+
+import io.quarkus.test.junit.QuarkusIntegrationTest;
+
+@QuarkusIntegrationTest
+class GreetingResourceIT extends GreetingResourceTest {
+    // Execute the same tests but in packaged mode.
+}
